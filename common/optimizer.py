810ed0b0e17f1e18a46d8406970a2ec83b502adb
Johannes Binder <j.binder.x@gmail.com>
2018-12-15 16:48:58 +0100

    Try different loss functions of none is specified

--- common/optimizer.py
+++ common/optimizer.py
@@ -5,16 +5,29 @@ from networks.network import Network
 
 
 class Optimizer:
+
+    def __init__(self):
+        self.loss_functions = ["L1Loss", "MSELoss"]
+
     def run(self, network: Network, data: dict, network_options: NetworkOptions, train_options: TrainOptions,
             optimizer_options: OptimizerOptions):
         best = {'loss': None}
-        for i in range(1, 11):
-            logging.info(f"Run #{i}...")
-            network.init(data, network_options, train_options)
-            network.train()
-            loss = network.validate()
-            if best['loss'] is None or loss < best['loss']:
-                best['loss'] = loss
-                if optimizer_options.save_path is not None:
-                    network.save(optimizer_options.save_path)
+        loss_functions = self.loss_functions if train_options.loss_function is None else [train_options.loss_function]
+        for loss_function in loss_functions:
+            for i in range(1, 11):
+                current_train_options = TrainOptions(
+                    train_options.num_epochs,
+                    train_options.print_every,
+                    train_options.use_gpu,
+                    train_options.optimizer,
+                    loss_function)
+                logging.info(f"Run #{i}: {current_train_options}...")
+                network.init(data, network_options, current_train_options)
+                network.train()
+                loss = network.validate()
+                if best['loss'] is None or loss < best['loss']:
+                    logging.info("New best run!")
+                    best['loss'] = loss
+                    if optimizer_options.save_path is not None:
+                        network.save(optimizer_options.save_path)
         return best
--- test/common/optimizer_test.py
+++ test/common/optimizer_test.py
@@ -1,15 +1,29 @@
+import logging
 import unittest
 from unittest.mock import Mock
 
 from common.optimizer import Optimizer
-from common.options import OptimizerOptions
+from common.options import OptimizerOptions, TrainOptions
 
 
 class OptimizerTest(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(__class__, self).__init__(*args, **kwargs)
+        logging.basicConfig(level=0)
+
     def test_run_should_call_network_multiple_times(self):
         network_mock = Mock()
         network_mock.validate.return_value = 1
         optimizer = Optimizer()
-        optimizer.run(network_mock, {}, None, None, OptimizerOptions(None))
+        optimizer.run(network_mock, {}, None, TrainOptions(None, None, None, None, "MSELoss"), OptimizerOptions(None))
         network_mock.init.assert_called()
         self.assertEqual(10, network_mock.init.call_count)
+
+    def test_run_no_loss_function_specified_should_enumerate_over_all_loss_functions(self):
+        network_mock = Mock()
+        network_mock.validate.return_value = 1
+        optimizer = Optimizer()
+        optimizer.run(network_mock, {}, None, TrainOptions(None, None, None, None, None), OptimizerOptions(None))
+        network_mock.init.assert_called()
+        self.assertEqual(20, network_mock.init.call_count)
